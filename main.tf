3426aab4f14371f1e07e07ebaa4bc67c81ecd942
GitHuberkata <petyagavrilova86@gmail.com>
2023-03-27 00:52:45 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "aws_network" {
-  source               = "git::https://github.com/slavrd/terraform-aws-basic-network.git?ref=0.4.1"
+  source               = "git::https://github.com/slavrd/terraform-aws-basic-network.git?ref=0.4.2"
   vpc_cidr_block       = var.vpc_cidr_block
   public_subnet_cidrs  = var.public_subnet_cidrs
   private_subnet_cidrs = var.private_subnet_cidrs
